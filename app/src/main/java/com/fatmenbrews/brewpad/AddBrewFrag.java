921736b30b8200235bdbae9621ab623ad7006e7d
jsandoval21 <juansndvl89@gmail.com>
2020-11-14 07:24:46 -0800

    Working with tab layouts and tab fragments

--- app/src/main/java/com/fatmenbrews/brewpad/AddBrewFrag.java
+++ app/src/main/java/com/fatmenbrews/brewpad/AddBrewFrag.java
@@ -11,12 +11,10 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
-public class AddBrewFragment extends Fragment {
+public class AddBrewFrag extends Fragment {
 
     private static final int NUM_PAGES = 6;
     private static final String[] tabNames = {"Brew Details", "Fermentables", "Mash", "Hops", "Yeast", "Water Chemistry"};
@@ -28,7 +26,7 @@ public class AddBrewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.add_brew_fragment, container, false);
         return view;
     }
 
@@ -36,13 +34,11 @@ public class AddBrewFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         viewPager = view.findViewById(R.id.tab_pager);
-        pagerAdapter = new TabSlidePagerAdapter(getActivity());
+        pagerAdapter = new TabSlidePagerAdapter(this);
         viewPager.setAdapter(pagerAdapter);
 
         TabLayout tabLayout = view.findViewById(R.id.brew_tabs);
-        for(String name : tabNames){
-            tabLayout.addTab(tabLayout.newTab().setText(name));
-        }
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> tab.setText(tabNames[position])).attach();
 
 //        view.findViewById(R.id.secondfragment_fab).setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -55,19 +51,45 @@ public class AddBrewFragment extends Fragment {
 
     private class TabSlidePagerAdapter extends FragmentStateAdapter{
 
-        public TabSlidePagerAdapter(@NonNull FragmentActivity fragmentActivity) {
-            super(fragmentActivity);
+        public TabSlidePagerAdapter(@NonNull Fragment fr) {
+            super(fr);
         }
 
         @NonNull
         @Override
         public Fragment createFragment(int position) {
-            return new TabFragment();
+            Fragment fragment = null;
+
+            switch (position){
+                case 0:
+                    fragment = new BrewDetailsFrag();
+                    break;
+                case 1:
+                    fragment = new FermentablesFrag();
+                    break;
+                case 2:
+                    fragment = new HopsFrag();
+                    break;
+                case 3:
+                    fragment = new MashFrag();
+                    break;
+                case 4:
+                    fragment = new YeastFrag();
+                    break;
+                case 5:
+                    fragment = new WaterProfileFrag();
+                    break;
+            }
+
+            fragment.getArguments();
+
+            return fragment;
         }
 
         @Override
         public int getItemCount() {
             return NUM_PAGES;
         }
+
     }
 }
--- app/src/main/java/com/fatmenbrews/brewpad/WaterProfileFrag.java
+++ app/src/main/java/com/fatmenbrews/brewpad/WaterProfileFrag.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class TabFragment extends Fragment {
+public class WaterProfileFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.tab_screen_slide_page, container, false);
+        View view = inflater.inflate(R.layout.water_profile, container, false);
         return view;
     }
 }
